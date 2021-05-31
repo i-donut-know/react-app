1ced0e861604af82aa3b04472380c66991949111
Kseniya <mylittleuniverse@list.ru>
2021-06-01 01:18:29 +0700

    создание компонентов приложения, их стили

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode>  
     <App />
   </React.StrictMode>,
   document.getElementById('root')
